53db9c79ef7c385cb12f3e9275a022daf5a03da7
User <katia@email.com>
2021-03-25 11:38:10 +0100

    fix navigation bar, create button for mobile navigation bar view, new JavaScript file to handle opening and closing mobile navigation view

---
+++ script.js
@@ -0,0 +1,8 @@
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+
+// show modal (mobile view navigation)
+open.addEventListener('click', () => modal.classList.add('show-modal'));
+
+// Hide modal (mobile view navigation)
+close.addEventListener('click', () => modal.classList.remove('show-modal'));
