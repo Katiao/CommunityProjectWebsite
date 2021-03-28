753df4c1e406f283e908a3b8901066d0902bd5f5
User <katia@email.com>
2021-03-28 12:42:30 +0200

    add fade-in animation upon scrolling page

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* mobile view navigation */
+
 const open = document.getElementById('open');
 const close = document.getElementById('close');
 
@@ -6,3 +8,25 @@ open.addEventListener('click', () => modal.classList.add('show-modal'));
 
 // Hide modal (mobile view navigation)
 close.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+/* scrolling animation */
+
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+	const triggerBottom = (window.innerHeight / 5) * 4;
+
+	boxes.forEach(box => {
+		const boxTop = box.getBoundingClientRect().top;
+
+		if (boxTop < triggerBottom) {
+			box.classList.add('show');
+		} else {
+			box.classList.remove('show');
+		}
+	});
+}
