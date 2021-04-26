ab328ca6d6b9627d981d2c6ca71f50023a2a5a57
Katia <katiao_@hotmail.co.uk>
2021-04-26 12:16:45 +0200

    add Form validation using JavaScript for international number format

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* mobile view navigation */
+/*                                mobile view navigation                                               */
 
 const open = document.getElementById('open');
 const close = document.getElementById('close');
@@ -8,3 +8,39 @@ open.addEventListener('click', () => modal.classList.add('show-modal'));
 
 // Hide modal (mobile view navigation)
 close.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+/*                                 Form validation - Phone number                                              */
+
+const form = document.getElementById('form');
+const phone = document.getElementById('number');
+
+//Show input error message
+function showError(input, message) {
+	const formControl = input.parentElement;
+	formControl.className = 'contact-form-number-control error';
+	const small = formControl.querySelector('small');
+	small.innerText = message;
+}
+
+//Show success outline
+function showSuccess(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'contact-form-number-control success';
+}
+
+//Regex international phone number validation
+function validateNumber() {
+	const re = /^\+(?:[0-9] ?){6,14}[0-9]$/;
+
+	if (!re.test(phone.value)) {
+		showError(phone, 'Add International Dialing code e.g. +49 408 XXX XXXX');
+	} else {
+		showSuccess(phone);
+	}
+}
+
+//Event listener
+form.addEventListener('submit', function (e) {
+	e.preventDefault();
+	validateNumber();
+});
