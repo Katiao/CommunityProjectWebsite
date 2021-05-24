2b6c269614890c943404d93038a50d99c89266f1
Katia <katiao_@hotmail.co.uk>
2021-05-24 11:03:31 +0200

    make back to top button disappear at top of page.

--- script.js
+++ script.js
@@ -17,6 +17,30 @@ document.querySelectorAll('.menu-nav').forEach((item) => {
 	});
 });
 
+/* ********************back to top button***********************************  */
+const backToTopBtn = document.querySelector('.back-to-top');
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+	scrollFunction();
+};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		backToTopBtn.classList.add('show');
+	} else {
+		backToTopBtn.classList.remove('show');
+	}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+	document.getElementById('hero').scrollIntoView({ behavior: 'smooth' });
+}
+
+//event listener
+backToTopBtn.addEventListener('click', topFunction);
+
 /* ********************* Form Validation ***************************************** */
 
 const form = document.getElementById('form');
