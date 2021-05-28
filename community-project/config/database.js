5b3a2ce1e92ea16b2f1e8939bcd4d7af567d3feb
Katia <katiao_@hotmail.co.uk>
2021-05-28 17:35:41 +0200

    set up remote API

--- community-project/config/database.js
+++ community-project/config/database.js
@@ -1,21 +1,41 @@
-module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "postgres",
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
-        username: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
-        schema: env("DATABASE_SCHEMA", "public"), // Not Required
-        ssl: {
-          rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+const parse = requie("pg-connection-string").parse;
+
+module.exports = ({ env }) => {
+  if (env("NODE_ENV") === "production") {
+    const config = parse(process.env.DATABASE_URL);
+    return {
+      defaultConnection: "default",
+      connections: {
+        default: {
+          connector: "bookshelf",
+          settings: {
+            client: "postgres",
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            username: config.user,
+            password: config.password,
+          },
+          options: {
+            ssl: false,
+          },
         },
       },
-      options: {},
+    };
+  }
+  return {
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "sqlite",
+          filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+        },
+        options: {
+          useNullAsDefault: true,
+        },
+      },
     },
-  },
-});
+  };
+};
--- communityProject/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'f24f002a-9cde-41b5-b596-f0f77627c0ec'
-};
